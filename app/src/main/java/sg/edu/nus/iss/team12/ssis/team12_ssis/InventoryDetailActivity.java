5982ca6315cae97940b7c729bf7e1b9c3d109d1b
thetnaingaye <mmu1tn@gmail.com>
2018-01-26 00:24:20 +0800

    added imagesfiles

--- app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryDetailActivity.java
+++ app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryDetailActivity.java
@@ -3,6 +3,7 @@ package sg.edu.nus.iss.team12.ssis.team12_ssis;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,15 +18,20 @@ public class InventoryDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_detail);
         ImageButton imgBtn = findViewById(R.id.imageButton_shelf);
+        String imgName = "h";
+        String imgLevel = "s1";
+        int id = getResourceId(imgName, "drawable", getPackageName());
+        final int levelId = getResourceId(imgLevel, "drawable", getPackageName());
+        imgBtn.setImageResource(id);
         imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 final Dialog d = new Dialog(InventoryDetailActivity.this);
                 d.setContentView(R.layout.layout_level);
                 ImageView imgLevel = d.findViewById(R.id.imageView_Level);
+                imgLevel.setImageResource(levelId);
                 TextView txtShelf = d.findViewById(R.id.textView_Shelf_Value);
-                txtShelf.setText("testing");
-                imgLevel.setImageResource(R.drawable.brandimg);
+                txtShelf.setText("H");
                 d.setCancelable(true);
 
                 d.show();
@@ -51,5 +57,15 @@ public class InventoryDetailActivity extends Activity {
 
             }
         });
+    }
+    //for getting image resource id from string
+    public int getResourceId(String pVariableName, String pResourcename, String pPackageName)
+    {
+        try {
+            return getResources().getIdentifier(pVariableName, pResourcename, pPackageName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
