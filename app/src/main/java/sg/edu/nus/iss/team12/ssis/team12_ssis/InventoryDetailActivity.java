52cc709b246f3fe158faf4b094cb53d0ad9014e2
thetnaingaye <mmu1tn@gmail.com>
2018-01-25 11:18:20 +0800

    added custom dialog layout for level location

--- app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryDetailActivity.java
+++ app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryDetailActivity.java
@@ -1,7 +1,12 @@
 package sg.edu.nus.iss.team12.ssis.team12_ssis;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class InventoryDetailActivity extends Activity {
 
@@ -9,5 +14,18 @@ public class InventoryDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_detail);
+        ImageButton imgBtn = findViewById(R.id.imageButton_shelf);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                final Dialog d = new Dialog(InventoryDetailActivity.this);
+                d.setContentView(R.layout.layout_level);
+                ImageView imgLevel = d.findViewById(R.id.imageView_Level);
+                imgLevel.setImageResource(R.drawable.brandimg);
+                d.setCancelable(true);
+
+                d.show();
+            }
+        });
     }
 }
