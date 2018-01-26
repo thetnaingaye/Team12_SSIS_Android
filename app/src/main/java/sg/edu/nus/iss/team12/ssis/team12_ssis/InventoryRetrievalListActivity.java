1a50971c547898277d1b6b68fb1abc513b0de935
thetnaingaye <mmu1tn@gmail.com>
2018-01-26 08:59:01 +0800

    added back to Main method

--- app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryRetrievalListActivity.java
+++ app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/InventoryRetrievalListActivity.java
@@ -1,7 +1,10 @@
 package sg.edu.nus.iss.team12.ssis.team12_ssis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InventoryRetrievalListActivity extends Activity {
 
@@ -9,5 +12,15 @@ public class InventoryRetrievalListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory_retrieval_list);
+
+        Button btnSave = findViewById(R.id.button_Save);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(InventoryRetrievalListActivity.this,MainActivity.class);
+                intent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/ViewDisbursementFormActivity.java
+++ app/src/main/java/sg/edu/nus/iss/team12/ssis/team12_ssis/ViewDisbursementFormActivity.java
@@ -1,7 +1,10 @@
 package sg.edu.nus.iss.team12.ssis.team12_ssis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewDisbursementFormActivity extends Activity {
 
@@ -11,5 +14,16 @@ public class ViewDisbursementFormActivity extends Activity {
         //Decide whether is it gonna be Sigature-based or PIN-based
         //Ouuuu Yeah~~!!!
         setContentView(R.layout.activity_view_disbursement_form);
+
+
+        Button btnSign = findViewById(R.id.button_Sign);
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(ViewDisbursementFormActivity.this,MainActivity.class);
+                intent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                startActivity(intent);
+            }
+        });
     }
 }
